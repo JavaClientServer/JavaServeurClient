d777e351e18528e6be01518fbb90b0e57c8b058d
Maxime Dito <dito.maxime@etu.unice.fr>
2014-11-05 11:56:27 +0100

    Mise en place du serveur :  - Réception de données  - envoie de données

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Class Server
@@ -10,27 +15,64 @@ import java.net.Socket;
  */
 public class Server {
 
+    private ObjectInputStream iStream = null;
+    private ObjectOutputStream oStream = null;
     private Socket socket = null;
 
-    public Server(Socket socket) {
-        this.socket = socket;
+    public Server(String ip, int portDest, int portSrc) throws UnknownHostException {
+        try {
+            this.socket = new Socket(ip, portDest, InetAddress.getLocalHost(), portSrc);
+            oStream = new ObjectOutputStream(socket.getOutputStream());
+            oStream.flush();
+            iStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void start() {
+    public Server(String ip) throws UnknownHostException {
+        try {
+            new Server(ip, 6969, 6969);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public boolean send(Serializable msg) {
+        try {
+            oStream.writeObject(msg);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isConnected() {
+        return iStream != null & oStream != null;
+}
+
+    public boolean receive() {
+
+        try {
+            Serializable msg = (Serializable) iStream.readObject();
+            System.out.println("J'ai reçu ceci :"+msg);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public static void main(String arg[]) throws IOException {
-        ServerSocket serverSocket = null;
-        boolean listening = true;
-        try {
-            serverSocket = new ServerSocket(8080);
 
-        } catch (IOException e) {
-            System.err.println("Impossible d'écouter le port 8080");
+        Server serv = new Server(arg[0]);
+        while(serv.isConnected()) {
+            if(serv.receive()) {
+                serv.send(new String("J'ai bien reçu copain !"));
+            }
         }
-        while(listening) new Server(serverSocket.accept()).start();
-        serverSocket.close();
 
     }
 
