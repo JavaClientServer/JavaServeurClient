183b82e4bcb4f42910af836af0527608fcc94607
Maxime Dito <dito.maxime@etu.unice.fr>
2014-11-05 11:01:11 +0100

    other commit

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,20 @@
 package server;
 
+import java.net.Socket;
+
 /**
  * Created by user on 05/11/2014.
  */
 public class Server {
+
+    private Socket socket = null;
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String arg[]) {
+        ServerSocket
+    }
+
 }
