f898d8c23f24e970625aff27dc9ffc25f8eb0896
Maxime Dito <dito.maxime@etu.unice.fr>
2014-11-09 10:39:05 +0100

    Nouvelle version du serveur : - multi Thread - patern observer

---
+++ src/message/Message.java
@@ -0,0 +1,29 @@
+package message;
+
+import java.io.Serializable;
+
+/**
+ Class Message
+ * Objet transmit entre le client et le serveur
+ */
+public class Message implements Serializable {
+    private String msg;
+    private static final long serialVersionUID = 69696969L;
+
+    public Message(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Message : "+this.msg;
+    }
+}
